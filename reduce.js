69560d581280ed4ffc9a18b4104fa98732e60d92
Hadi Raza <hadiraza.edu@gmail.com>
2023-08-28 20:09:38 +0000

    reduce array method

--- reduce.js
+++ reduce.js
@@ -1,7 +1,7 @@
 const nums = [1, 2, 3, 6];
 
-const sumNums = nums.reduce( (acc, curr) => acc * curr, 1);
-// console.log(sumNums);
+const sumNums = nums.reduce( (acc, curr) => acc + curr, 0);
+console.log(sumNums); // 12
 
 const shoppingCart = [
     {
@@ -21,6 +21,9 @@ const shoppingCart = [
         price: "12999"
     }
 ]
-
 const priceToPay = shoppingCart.reduce( (acc, item) => acc + Number(item.price), 0);
-console.log(priceToPay);
+console.log(priceToPay); // 29996
+
+const yourPrice = shoppingCart.reduce( (acc, curr) => acc + Number(curr.price), 0)
+console.log(`Your price is: ${yourPrice}. Your total price with include of Rs: 90 tax  is Rs: ${yourPrice + 10}.`);
+// Your price is: 29996. Your total price with include of Rs: 90 tax  is Rs: 30006.
