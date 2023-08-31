87c2ec1004e8423266a6116bf81b9f0879e2ace9
Hadi Raza <hadiraza.edu@gmail.com>
2023-08-31 20:32:18 +0000

    prefix & postfix

---
+++ dataTypes_Conversion/operations.js
@@ -0,0 +1,12 @@
+//console.log(2+2);  // 4
+//console.log(2-1);  // 3
+//console.log(2*7);  // 14
+//console.log(12/2);  // 6
+
+//console.log(1 + "2");     // 12
+//console.log("1" + 2);     // 12
+//console.log(1 + 8 + "2")  // 92
+//console.log("2" + 5 + 7)  // 257
+
+//console.log(Number(true));  // 1
+//console.log(+true);         // 1
---
+++ dataTypes_Conversion/prefix&postfix.js
@@ -0,0 +1,12 @@
+// READ DOCUMENTATION FROM HERE https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let a = 10;
+let b = a++;
+console.log(a); // 11
+console.log(b); // 10
+
+
+let c = 30;
+let d = ++c;
+console.log(c); // 31
+console.log(d); // 31
