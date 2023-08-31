9bb28989dedabfb916dfb2bb14a5ce8345fad509
Hadi Raza <hadiraza.edu@gmail.com>
2023-08-31 09:35:41 +0000

    dataTypes conversion

--- dataTypes_Conversion/conversion.js
+++ dataTypes_Conversion/conversion.js
@@ -16,5 +16,37 @@ let isMaleNum = Number(isMale)
 
 let isDefined = undefined;
 let isDefinedNum = Number(isDefined);
-console.log(typeof(isDefined));       // undefined
-console.log(isDefinedNum);            // NaN
+//console.log(typeof(isDefined));       // undefined
+//console.log(isDefinedNum);            // NaN
+
+// Convert When Number Method is applied
+// "33"       =>    33
+// undefined  =>    NaN
+// "33abc"    =>    NaN
+// true       =>    1
+// false      =>    0
+// null       =>    0
+
+
+let isLoggedIn = 1;
+let isLoggedInBool = Boolean(isLoggedIn);
+//console.log(isLoggedInBool);            // true
+
+let emptyStr = "";
+let emptyStrBool = Boolean(emptyStr);
+//console.log(emptyStrBool);              // false
+
+let string = "HelloWorld!"
+let stringBool = Boolean(string);
+//console.log(stringBool);                // true
+
+// Convert When Boolear Method is applied
+// ""           =>      false
+// "Hello"      =>      true
+// 1            =>      true
+// 0            =>      false
+
+let someNum = 123;
+let stringNum = String(someNum);
+console.log(stringNum);                  // 123
+console.log(typeof stringNum);           // string
